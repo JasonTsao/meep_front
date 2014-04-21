1991fe0773cb4121a5e9124bf0ab9e3ad48b980b
ryansharp <rtsharp90@gmail.com>
2014-04-21 11:49:42 -0700

    Initial commit from local git

---
+++ Meep_IOS/MEPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MEPAppDelegate.h
+//  Meep_IOS
+//
+//  Created by Ryan Sharp on 4/21/14.
+//  Copyright (c) 2014 futoi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MEPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Meep_IOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Meep_IOS
+//
+//  Created by Ryan Sharp on 4/21/14.
+//  Copyright (c) 2014 futoi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MEPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MEPAppDelegate class]));
+    }
+}
