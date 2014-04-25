d7dc848aff8f34fafecd6e34a83e28068092286b
Jason Tsao <jason@Jasons-Mac.local>
2014-04-24 19:07:16 -0700

    adding friend list files to project

---
+++ Meep_IOS/Friend.h
@@ -0,0 +1,31 @@
+//
+//  Friend.h
+//  Friends
+//
+//  Created by Jason Tsao on 4/24/14.
+//  Copyright (c) 2014 Jason Tsao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Friend : NSObject <NSCoding>{
+    NSInteger account_id;
+    NSString *name;
+    NSString *bio;
+    NSString *imageFileName;
+    NSString *phoneNumber;
+    NSInteger numFriends;
+    NSInteger numTimesInvitedByMe;
+}
+
+@property (nonatomic) NSInteger account_id;
+@property (nonatomic) NSString *name;
+@property (nonatomic) NSString *bio;
+@property (nonatomic) NSString *imageFileName;
+@property (nonatomic) NSString *phoneNumber;
+@property (nonatomic) NSInteger numFriends;
+@property (nonatomic) NSInteger numTimesInvitedByMe;
+
+
+
+@end
